e1e818668717e8b4eab3e6904e9529fe7f50511d
Isaac Martinez <isaac.martinez@mastercorp.com>
2020-01-08 22:09:13 -0500

    Updating NRSRx libraries

--- src/MicroServices/Competencies/src/Tests/IkeMtz.NRSRx.Competencies.OData.Tests/Integration/OData/CompetenciesTests.cs
+++ src/MicroServices/Competencies/src/Tests/IkeMtz.NRSRx.Competencies.OData.Tests/Integration/OData/CompetenciesTests.cs
@@ -10,30 +10,28 @@ using System.Threading.Tasks;
 
 namespace IkeMtz.NRSRx.Competencies.Tests.Integration.OData
 {
-    [TestClass]
-    public partial class CompetenciesTests : BaseUnigrationTests
+  [TestClass]
+  public partial class CompetenciesTests : BaseUnigrationTests
+  {
+    [TestMethod]
+    [TestCategory("Integration")]
+    public async Task GetEnabledCompetenciesTest()
     {
-        [TestMethod]
-        [TestCategory("Integration")]
-        public async Task GetEnabledCompetenciesTest()
-        {
-            using (var srv = new TestServer(TestHostBuilder<Startup, IntegrationTestStartup>()))
-            {
-                var client = srv.CreateClient();
-                GenerateAuthHeader(client, await GenerateTokenAsync());
+      using var srv = new TestServer(TestHostBuilder<Startup, IntegrationTestStartup>());
+      var client = srv.CreateClient();
+      GenerateAuthHeader(client, await GenerateTokenAsync());
 
-                var resp = await client.GetStringAsync($"odata/v1/{nameof(Competencies)}?$count=true");
-                TestContext.WriteLine($"Server Reponse: {resp}");
-                var envelope = JsonConvert.DeserializeObject<ODataEnvelope<Competency>>(resp);
-                Assert.AreEqual(envelope.Count, envelope.Value.Count());
-                envelope.Value.ToList().ForEach(t =>
-                {
-                    Assert.IsNotNull(t.CreatedBy);
-                    Assert.IsNotNull(t.CreatedOnUtc);
+      var resp = await client.GetStringAsync($"odata/v1/{nameof(Competencies)}?$count=true");
+      TestContext.WriteLine($"Server Reponse: {resp}");
+      var envelope = JsonConvert.DeserializeObject<ODataEnvelope<Competency>>(resp);
+      Assert.AreEqual(envelope.Count, envelope.Value.Count());
+      envelope.Value.ToList().ForEach(t =>
+      {
+        Assert.IsNotNull(t.CreatedBy);
+        Assert.IsNotNull(t.CreatedOnUtc);
 
-                    Assert.IsTrue(t.IsEnabled);
-                });
-            }
-        }
+        Assert.IsTrue(t.IsEnabled);
+      });
     }
+  }
 }
