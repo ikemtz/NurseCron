00f502af526ed21a72d8a4ed169166d1de04ec1d
IkeMtz <ikemtz@outlook.com>
2020-05-23 14:30:51 -0400

    Applying project Guids

--- sonarqube-create-project-guids.ps1
+++ sonarqube-create-project-guids.ps1
@@ -1,4 +1,13 @@
-$paths = Get-ChildItem -Path $PSScriptRoot -include *.csproj -Recurse
+# Project Guid Generator
+# Creator: Unknown
+# Editor: Isaac martinez
+# Notes: A requirement of SonarCloud/SonarQube is that all projects have a unique
+#   ProjectGuid.  This script takes care of that.  This script will recursively
+#   look for projects in subfolders and apply the necessary changes.
+# Version Date: 05/19/2020
+
+$paths = Get-ChildItem -Path $PSScriptRoot -include *proj -Recurse
+Write-Host $paths.count "project(s) found"
 $projectGuids = @()
 foreach ($pathobject in $paths) {
     $path = $pathobject.fullname
@@ -9,22 +18,23 @@ foreach ($pathobject in $paths) {
         $child = $doc.CreateElement("ProjectGuid")
         $child.InnerText = [guid]::NewGuid().ToString().ToUpper()
         $node = $doc.SelectSingleNode("//Project/PropertyGroup")
-        $node.AppendChild($child)
-        $doc.Save($path)
-        $projectGuids += $child.InnerText.ToUpper()
+        if (!$node){        
+            Write-Host "Error:" $path "- Project file is missing //Project/PropertyGroup node"
+        }
+        else {
+            $node.AppendChild($child)
+            $doc.Save($path)
+            $projectGuids += $child.InnerText.ToUpper()
+        }
     }
     elseif($projectGuids.Contains($node.InnerText)){
         Write-Host "Duplicate found"
         $node.InnerText = [guid]::NewGuid().ToString().ToUpper()
-        $doc.Save($path)
-        
-    $projectGuids += $node.InnerText.ToUpper();
+        $doc.Save($path)        
+        $projectGuids += $node.InnerText.ToUpper();
     }
     else{
-    $projectGuids += $node.InnerText;
+        $projectGuids += $node.InnerText;
+        Write-Host $path "already has a valid ProjectGuid"
     }
-}
-foreach($g in $projectGuids | Sort-Object)
-{
-Write-Host $g
 }
